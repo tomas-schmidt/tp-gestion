9450b5aa9f886f2755bad90f0914d3f4b1012166
tomas-schmidt <tomi_95@hotmail.com>
2016-06-03 20:41:32 -0300

    agrego nueva tabla para tipo_doc y migro tabla cliente

--- Migracion.sql
+++ Migracion.sql
@@ -2,181 +2,282 @@
 --						CREAR ESQUEMA
 /****************************************************************/
 
-USE GD1C2016
-GO
 CREATE SCHEMA C_HASHTAG AUTHORIZATION gd
-
-CREATE TABLE Rol
-(
-Id_Rol numeric(18,0) PRIMARY KEY,
-Nombre_Rol nvarchar(255),
-Habilitado bit 
-)
-
-CREATE TABLE Funcionalidad
-(
-Id_Funcionalidad numeric(18,0) PRIMARY KEY,
-Nombre_Funcionalidad nvarchar(255) 
-)
-
-CREATE TABLE Funcionalidad_Rol
-(
-Id_Rol numeric(18,0) FOREIGN KEY REFERENCES Rol(Id_Rol),
-Id_Funcionalidad numeric(18,0) FOREIGN KEY REFERENCES Funcionalidad(Id_Funcionalidad) 
-)
-
-CREATE TABLE Cliente
-(
-Id_Cliente numeric(18,0) PRIMARY KEY,
-Nombre nvarchar(255),
-Apellido nvarchar(255),
-DNI numeric(18,0),
-Tipo_Doc nvarchar(255),
-Mail nvarchar(255),
-Telefono numeric(18,0),
-Domicilio_Calle nvarchar(100),
-Nro_Calle numeric(18,0),
-Piso numeric(18,0),
-Departamento nvarchar(50),
-Cod_Postal nvarchar(50),
-Nacimiento datetime,
-Creacion datetime
-)
-
-CREATE TABLE Rubro
-(
-Id_Rubro numeric(18,0) PRIMARY KEY,
-Desc_Corta nvarchar(50),
-Desc_Larga nvarchar(255)
-)
-
-CREATE TABLE Empresa
-(
-Id_Empresa numeric(18,0) PRIMARY KEY,
-Razon_Social nvarchar(255),
-Mail nvarchar(50),
-Telefono numeric(18,0),
-Calle nvarchar(100),
-Cod_Postal nvarchar(50),
-Ciudad nvarchar(255),
-Cuit nvarchar(50),
-Nombre_Contacto nvarchar(255),
-Rubro_Principal numeric(18,0) FOREIGN KEY REFERENCES Rubro(Id_Rubro),
-Nro_Calle numeric(18,0),
-Piso numeric(18,0),
-Departamento nvarchar(50)
-)
-
-CREATE TABLE Estado
-(
-Id_Estado numeric(18,0) PRIMARY KEY,
-Descripcion nvarchar(255) 
-)
-
-CREATE TABLE Visibilidad
-(
-Id_Visibilidad numeric(18,0) PRIMARY KEY,
-Comision_Prod_Vend numeric(18,2),
-Comision_Envio_Prod numeric(18,2),
-Comision_Tipo_Public numeric(18,2)
---Importacia ,								QUE SIGNIFICA IMPORTANCIA?
-)
-
-CREATE TABLE Usuario
-(
-Id_User numeric(18,0) PRIMARY KEY,
-Username nvarchar(50),
-Password nvarchar(50),
-Intentos_Fallidos numeric(18,0),
-Estado bit,
-Id_Empresa numeric(18,0) FOREIGN KEY REFERENCES Empresa(Id_Empresa),
-Id_Cliente numeric(18,0) FOREIGN KEY REFERENCES Cliente(Id_Cliente)
-)
-
-CREATE TABLE Tipo
-(
-Id_Tipo numeric(18,0) PRIMARY KEY,
-Descripcion nvarchar(255) 
-)
-
-CREATE TABLE Publicacion
-(
-Id_Publicacion numeric(18,0) PRIMARY KEY,
-Monto numeric(18,2),
-Id_Visibilidad numeric(18,0) FOREIGN KEY REFERENCES Visibilidad(Id_Visibilidad),
-Id_User numeric(18,0) FOREIGN KEY REFERENCES Usuario(Id_User),
-Id_Estado numeric(18,0) FOREIGN KEY REFERENCES Estado(Id_Estado),
-Id_Tipo numeric(18,0) FOREIGN KEY REFERENCES Tipo(Id_Tipo), 
-Tipo nvarchar(255),
-Fecha_Inicial datetime,
-Fecha_Final datetime,
-Preguntas nvarchar(255),
-Stock numeric(18,0),
-Descripcion nvarchar(255)
-)
-
-
-
-CREATE TABLE Compra
-(
-Id_Compra numeric(18,0) PRIMARY KEY,
-Id_User numeric(18,0) FOREIGN KEY REFERENCES Usuario(Id_User),
-Id_Publicacion numeric(18,0) FOREIGN KEY REFERENCES Publicacion(Id_Publicacion),
-Monto numeric(18,2),
-Fecha datetime
-)
-
-CREATE TABLE Factura
-(
-Id_Factura numeric(18,0) PRIMARY KEY,
-Id_Publicacion numeric(18,0) FOREIGN KEY REFERENCES Publicacion(Id_Publicacion)
-)
-
-CREATE TABLE Item
-(
-Id_Item numeric(18,0) PRIMARY KEY,
-Id_Factura numeric(18,0) FOREIGN KEY REFERENCES Factura(Id_Factura),
-Descripcion nvarchar(255),
-Monto numeric(18,2)
-)
-
-CREATE TABLE Calificacion
-(
-Id_Calificacion numeric(18,0) PRIMARY KEY,
-Cant_Estrellas numeric(18,0),
-Descripcion nvarchar(255),
-Id_Compra numeric(18,0) FOREIGN KEY REFERENCES Compra(Id_Compra)
-)
-
-CREATE TABLE Oferta
-(
-Id_Oferta numeric(18,0) PRIMARY KEY,
-Id_Publicacion numeric(18,0) FOREIGN KEY REFERENCES Publicacion(Id_Publicacion), 
-Id_User numeric(18,0) FOREIGN KEY REFERENCES Usuario(Id_User),
-Monto_Ofertado numeric(18,2),
-Fecha datetime 
-)
-
-CREATE TABLE Rubro_Publicacion
-(
-Id_Rubro numeric(18,0) FOREIGN KEY REFERENCES Rubro(Id_Rubro),
-Id_Publicacion numeric(18,0) FOREIGN KEY REFERENCES Publicacion(Id_Publicacion)
-)
-
-
-CREATE TABLE Rol_Usuario
-(
-Id_User numeric(18,0) FOREIGN KEY REFERENCES Usuario(Id_User),
-Id_Rol numeric(18,0) FOREIGN KEY REFERENCES Rol(Id_Rol)
-)
-
 GO
-
 
 /***********************************************************************
  *
  *						MIGRACION DE DATOS
  *
  ***********************************************************************/
- 
- 
+
+USE GD1C2016
+
+
+/****************************************************************/
+--							ROL
+/****************************************************************/
+CREATE TABLE C_HASHTAG.Rol
+(
+	Id_Rol numeric(18,0) IDENTITY(1,1) PRIMARY KEY,
+	Nombre_Rol nvarchar(255),
+	Habilitado bit 
+)
+
+/****************************************************************/
+--							Funcionalidad
+/****************************************************************/
+CREATE TABLE C_HASHTAG.Funcionalidad
+(
+	Id_Funcionalidad numeric(18,0)IDENTITY(1,1) PRIMARY KEY,
+	Nombre_Funcionalidad nvarchar(255) 
+)
+
+
+/****************************************************************/
+--							Funcionalidad_Rol
+/****************************************************************/
+CREATE TABLE C_HASHTAG.Funcionalidad_Rol
+(
+	Id_Rol numeric(18,0) FOREIGN KEY REFERENCES C_HASHTAG.Rol(Id_Rol),
+	Id_Funcionalidad numeric(18,0) FOREIGN KEY REFERENCES C_HASHTAG.Funcionalidad(Id_Funcionalidad) 
+	CONSTRAINT PK_Funcionalidad_Rol PRIMARY KEY CLUSTERED 
+)
+
+/****************************************************************/
+--						DOCUMENTO
+/****************************************************************/
+CREATE TABLE C_HASHTAG.Tipo_Doc(
+	Doc_Codigo numeric(18,0) IDENTITY(1,1) PRIMARY KEY,
+	Doc_Desc varchar(255) NOT NULL
+)
+		
+INSERT INTO C_HASHTAG.Tipo_Doc(Doc_Desc) VALUES ('DNI')
+INSERT INTO C_HASHTAG.Tipo_Doc(Doc_Desc) VALUES ('Cedula')
+
+
+/****************************************************************/
+--							Cliente
+/****************************************************************/
+CREATE TABLE C_HASHTAG.Cliente
+(
+	Id_Cliente numeric(18,0) IDENTITY(1,1) PRIMARY KEY,
+	Nombre nvarchar(255),
+	Apellido nvarchar(255),
+	Nro_Doc numeric(18,0),
+	Tipo_Doc numeric(18,0) FOREIGN KEY REFERENCES C_HASHTAG.Tipo_doc(Doc_Codigo),
+	Mail nvarchar(255),
+	Telefono numeric(18,0),
+	Domicilio_Calle nvarchar(100),
+	Nro_Calle numeric(18,0),
+	Piso numeric(18,0),
+	Departamento nvarchar(50),
+	Cod_Postal nvarchar(50),
+	Nacimiento datetime,
+	Creacion datetime
+)
+
+INSERT INTO C_HASHTAG.Cliente(
+	Nombre,
+	Apellido,
+	Nro_Doc,
+	Tipo_Doc,
+	Mail,
+	Telefono,
+	Domicilio_Calle,
+	Nro_Calle,
+	Piso,
+	Departamento,
+	Cod_Postal,
+	Nacimiento,
+	Creacion 
+	)
+	SELECT DISTINCT
+		Cli_Nombre,
+		Cli_Apeliido,
+		Cli_Dni,
+		1,
+		Cli_Mail,
+		NULL,
+		Cli_Dom_Calle,
+		Cli_Nro_Calle,
+		Cli_Piso,
+		Cli_Depto,
+		Cli_Cod_Postal,
+		Cli_Fecha_Nac,
+		NULL -- falta obtener la fecha de creacion
+		FROM gd_esquema.Maestra
+		where Cli_Nombre IS NOT NULL
+		
+
+/****************************************************************/
+--							Rubro
+/****************************************************************/
+CREATE TABLE C_HASHTAG.Rubro
+(
+	Id_Rubro numeric(18,0) IDENTITY(1,1) PRIMARY KEY,
+	Desc_Corta nvarchar(50),
+	Desc_Larga nvarchar(255)
+)
+
+/****************************************************************/
+--							Empresa
+/****************************************************************/
+CREATE TABLE C_HASHTAG.Empresa
+(
+	Id_Empresa numeric(18,0) IDENTITY(1,1) PRIMARY KEY,
+	Razon_Social nvarchar(255),
+	Mail nvarchar(50),
+	Telefono numeric(18,0),
+	Calle nvarchar(100),
+	Cod_Postal nvarchar(50),
+	Ciudad nvarchar(255),
+	Cuit nvarchar(50),
+	Nombre_Contacto nvarchar(255),
+	Rubro_Principal numeric(18,0) FOREIGN KEY REFERENCES C_HASHTAG.Rubro(Id_Rubro),
+	Nro_Calle numeric(18,0),
+	Piso numeric(18,0),
+	Departamento nvarchar(50)
+)
+
+/****************************************************************/
+--							Estado
+/****************************************************************/
+CREATE TABLE C_HASHTAG.Estado
+(
+	Id_Estado numeric(18,0) IDENTITY(1,1) PRIMARY KEY,
+	Descripcion nvarchar(255) 
+)
+
+/****************************************************************/
+--							Visibilidad
+/****************************************************************/
+CREATE TABLE C_HASHTAG.Visibilidad
+(
+	Id_Visibilidad numeric(18,0) IDENTITY(1,1) PRIMARY KEY,
+	Comision_Prod_Vend numeric(18,2),
+	Comision_Envio_Prod numeric(18,2),
+	Comision_Tipo_Public numeric(18,2)
+	--Importacia ,								QUE SIGNIFICA IMPORTANCIA?
+)
+
+/****************************************************************/
+--							Usuario
+/****************************************************************/
+CREATE TABLE C_HASHTAG.Usuario
+(
+	Id_User numeric(18,0) IDENTITY(1,1) PRIMARY KEY,
+	Username nvarchar(50),
+	Password nvarchar(50),
+	Intentos_Fallidos numeric(18,0),
+	Estado bit,
+	Id_Empresa numeric(18,0) FOREIGN KEY REFERENCES C_HASHTAG.Empresa(Id_Empresa),
+	Id_Cliente numeric(18,0) FOREIGN KEY REFERENCES C_HASHTAG.Cliente(Id_Cliente)
+)
+
+/****************************************************************/
+--							Tipo
+/****************************************************************/
+CREATE TABLE C_HASHTAG.Tipo
+(
+	Id_Tipo numeric(18,0) IDENTITY(1,1) PRIMARY KEY,
+	Descripcion nvarchar(255) 
+)
+
+/****************************************************************/
+--							Publicacion
+/****************************************************************/
+CREATE TABLE C_HASHTAG.Publicacion
+(
+	Id_Publicacion numeric(18,0) PRIMARY KEY,
+	Monto numeric(18,2),
+	Id_Visibilidad numeric(18,0) FOREIGN KEY REFERENCES C_HASHTAG.Visibilidad(Id_Visibilidad),
+	Id_User numeric(18,0) FOREIGN KEY REFERENCES C_HASHTAG.Usuario(Id_User),
+	Id_Estado numeric(18,0) FOREIGN KEY REFERENCES C_HASHTAG.Estado(Id_Estado),
+	Id_Tipo numeric(18,0) FOREIGN KEY REFERENCES C_HASHTAG.Tipo(Id_Tipo), 
+	Tipo nvarchar(255),
+	Fecha_Inicial datetime,
+	Fecha_Final datetime,
+	Preguntas nvarchar(255),
+	Stock numeric(18,0),
+	Descripcion nvarchar(255)
+)
+
+/****************************************************************/
+--							Compra
+/****************************************************************/
+CREATE TABLE C_HASHTAG.Compra
+(
+	Id_Compra numeric(18,0) IDENTITY(1,1) PRIMARY KEY,
+	Id_User numeric(18,0) FOREIGN KEY REFERENCES C_HASHTAG.Usuario(Id_User),
+	Id_Publicacion numeric(18,0) FOREIGN KEY REFERENCES C_HASHTAG.Publicacion(Id_Publicacion),
+	Monto numeric(18,2),
+	Fecha datetime
+)
+
+/****************************************************************/
+--							Factura
+/****************************************************************/
+CREATE TABLE C_HASHTAG.Factura
+(
+	Id_Factura numeric(18,0) IDENTITY(1,1) PRIMARY KEY,
+	Id_Publicacion numeric(18,0) FOREIGN KEY REFERENCES C_HASHTAG.Publicacion(Id_Publicacion)
+)
+
+/****************************************************************/
+--							Item
+/****************************************************************/
+CREATE TABLE C_HASHTAG.Item
+(
+	Id_Item numeric(18,0) IDENTITY(1,1) PRIMARY KEY,
+	Id_Factura numeric(18,0) FOREIGN KEY REFERENCES C_HASHTAG.Factura(Id_Factura),
+	Descripcion nvarchar(255),
+	Monto numeric(18,2)
+)
+
+
+/****************************************************************/
+--							Calificacion
+/****************************************************************/
+CREATE TABLE C_HASHTAG.Calificacion
+(
+	Id_Calificacion numeric(18,0) IDENTITY(1,1) PRIMARY KEY,
+	Cant_Estrellas numeric(18,0),
+	Descripcion nvarchar(255),
+	Id_Compra numeric(18,0) FOREIGN KEY REFERENCES C_HASHTAG.Compra(Id_Compra)
+)
+
+/****************************************************************/
+--							Oferta
+/****************************************************************/
+CREATE TABLE C_HASHTAG.Oferta
+(
+	Id_Oferta numeric(18,0) IDENTITY(1,1) PRIMARY KEY,
+	Id_Publicacion numeric(18,0) FOREIGN KEY REFERENCES C_HASHTAG.Publicacion(Id_Publicacion), 
+	Id_User numeric(18,0) FOREIGN KEY REFERENCES C_HASHTAG.Usuario(Id_User),
+	Monto_Ofertado numeric(18,2),
+	Fecha datetime 
+)
+
+/****************************************************************/
+--							Rubro_Publicacion
+/****************************************************************/
+CREATE TABLE C_HASHTAG.Rubro_Publicacion
+(
+	Id_Rubro numeric(18,0) FOREIGN KEY REFERENCES C_HASHTAG.Rubro(Id_Rubro),
+	Id_Publicacion numeric(18,0) FOREIGN KEY REFERENCES C_HASHTAG.Publicacion(Id_Publicacion)
+	CONSTRAINT PK_Rubro_Publicacion PRIMARY KEY CLUSTERED
+)
+
+/****************************************************************/
+--							Rol_Usuario
+/****************************************************************/
+CREATE TABLE C_HASHTAG.Rol_Usuario
+(
+	Id_User numeric(18,0) FOREIGN KEY REFERENCES C_HASHTAG.Usuario(Id_User),
+	Id_Rol numeric(18,0) FOREIGN KEY REFERENCES C_HASHTAG.Rol(Id_Rol)
+	CONSTRAINT PK_Rol_Usuario PRIMARY KEY CLUSTERED
+)
+
+
+
